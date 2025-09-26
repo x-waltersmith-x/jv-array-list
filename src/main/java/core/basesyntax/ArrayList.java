378b4c678860de97eda49c24ed1bcf3531d6154f
x-waltersmith-x <vk.iblogic@gmail.com>
2025-09-26 13:36:13 +0200

    Fixes pt 2

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
     private static final int DEFAULT_CAPACITY = 10;
     // Growth factor 1.5x expressed as 3/2 to avoid magic numbers
